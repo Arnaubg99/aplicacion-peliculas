1132eb405197580f935c546f39cffdab5d7678d1
Arnaubg99 <arnaubayo99@gmail.com>
2023-10-30 19:48:01 +0100

    feat: maquetacion basica inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'inicio',
+    loadChildren: () => import('./home/home.module').then(modulo => modulo.HomeModule),
+    title: 'Inicio'
+  },
+  {
+    path: 'favoritos',
+    loadChildren: () => import('./favoritos/favoritos.module').then(modulo => modulo.FavoritosModule),
+    title: 'Películas favoritos'
+  },
+  {
+    path: '**',
+    redirectTo: 'inicio'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
